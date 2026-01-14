0604478c1209fde295af68f376d456de88dc461b
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-14 15:31:45 -0300

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,13 +8,9 @@ describe('Funcionalidade Login', () => {
 
     it('Login com sucesso usando Comando customizado', () => {
         cy.login(perfil.usuario, perfil.senha)
-        
-        // Espera o menu lateral de endereços estar visível antes de clicar
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a', { timeout: 15000 })
+        // No WooCommerce, o link de Logout confirma que você está logado
+        cy.get('.woocommerce-MyAccount-navigation-link--customer-logout a', {timeout: 20000})
           .should('be.visible')
-          .click()
-        
-        cy.get('.woocommerce-column__title').should('contain', 'Endereço de faturamento')
     });
 
     it('Login usando fixture', () => {
@@ -22,14 +18,8 @@ describe('Funcionalidade Login', () => {
             cy.get('#username').type(dados.usuario)
             cy.get('#password').type(dados.senha, { log: false })
             cy.get('.woocommerce-form > .button').click()
-            cy.get('.woocommerce-MyAccount-content', { timeout: 15000 }).should('be.visible')
+            cy.get('.woocommerce-MyAccount-navigation-link--customer-logout a', {timeout: 20000})
+              .should('be.visible')
         })
-    });
-
-    it('Deve fazer login com sucesso - sem otimização', () => {
-        cy.get('#username').type(perfil.usuario)
-        cy.get('#password').type(perfil.senha, { log: false })
-        cy.get('.woocommerce-form > .button').click()
-        cy.get('.page-title').should('contain', 'Minha conta')
     });
 });
