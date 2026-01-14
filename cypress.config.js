00d1920191c0b3c77aed83e5da991a51e8e7a303
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-13 22:16:51 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    // Aumentamos para 10 segundos porque o Jenkins/Servidor pode oscilar
+    defaultCommandTimeout: 10000, 
+    pageLoadTimeout: 30000,
+    viewportWidth: 1280,
+    viewportHeight: 720,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://lojaebac.ebaconline.art.br/"
   },
 });
