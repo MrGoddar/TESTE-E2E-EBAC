311fa7c898fb9a37adf682476c17811814c95a02
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-13 22:31:00 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,7 @@
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    cy.get('#password').type(senha, { log: false })
     cy.get('.woocommerce-form > .button').click()
-});
 
+    cy.get('.page-title', { timeout: 15000 }).should('contain', 'Minha conta')
+});
