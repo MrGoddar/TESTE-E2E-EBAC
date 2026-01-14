e997cb0b5592a059c8335582feff25296a58258a
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-14 15:25:09 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,6 @@
 Cypress.Commands.add('login', (usuario, senha) => {
-    cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, { log: false })
-    cy.get('.woocommerce-form > .button').click()
-
-    // Isso garante que o Cypress espere o painel carregar antes de seguir
-    cy.get('.woocommerce-MyAccount-content', { timeout: 20000 }).should('be.visible')
+    cy.visit('minha-conta')
+    cy.get('#username').type(usuario, {force: true})
+    cy.get('#password').type(senha, {log: false, force: true})
+    cy.get('.woocommerce-form > .button').click({force: true})
 });
