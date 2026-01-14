1c732343500ee8aa8efe4a43ea60c58fb4604389
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-14 16:18:39 -0300

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,25 +1,24 @@
-/// <reference types="cypress" />
-const perfil = require('../fixtures/perfil.json')
-
 describe('Funcionalidade Login', () => {
+
     beforeEach(() => {
-        cy.visit('minha-conta')
+        cy.visit('minha-conta/');
     });
 
     it('Login com sucesso usando Comando customizado', () => {
-        cy.login(perfil.usuario, perfil.senha)
-        // No WooCommerce, o link de Logout confirma que você está logado
-        cy.get('.woocommerce-MyAccount-navigation-link--customer-logout a', {timeout: 20000})
-          .should('be.visible')
+        // Substitua pelos seus dados válidos
+        cy.login('aluno_ebac@teste.com', 'teste@teste.com'); 
+        
+        // Verifica se o menu lateral de conta apareceu
+        cy.get('.woocommerce-MyAccount-navigation').should('be.visible');
+        
+        // Verifica se o link de Sair existe (indica que logou)
+        cy.contains('Sair').should('be.visible');
     });
 
     it('Login usando fixture', () => {
         cy.fixture('perfil').then(dados => {
-            cy.get('#username').type(dados.usuario)
-            cy.get('#password').type(dados.senha, { log: false })
-            cy.get('.woocommerce-form > .button').click()
-            cy.get('.woocommerce-MyAccount-navigation-link--customer-logout a', {timeout: 20000})
-              .should('be.visible')
-        })
+            cy.login(dados.usuario, dados.senha);
+        });
+        cy.contains('Sair').should('be.visible');
     });
 });
