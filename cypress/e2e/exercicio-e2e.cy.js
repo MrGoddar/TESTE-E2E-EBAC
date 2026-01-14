05dc48901093c645f3341b4698b5b4d31be18f27
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-14 17:27:47 -0300

    Update exercicio-e2e.cy.js

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,31 +1,34 @@
 describe('Exercicio - Testes End-to-end', () => {
-    it('Deve fazer um pedido de ponta a ponta', () => {
+    
+    beforeEach(() => {
         cy.visit('produtos/');
+    });
 
-        // Seleção de produto estável
+    it('Deve fazer um pedido de ponta a ponta', () => {
+        // Seleção do produto
         cy.get('.product-block').first().click();
         cy.get('.button-variable-item-S').click();
         cy.get('.button-variable-item-Blue').click();
         cy.get('.single_add_to_cart_button').click();
 
-        // Correção: Aguarda a mensagem de sucesso aparecer antes de buscar o botão
-        // Isso evita o erro de "content: Ver carrinho but never did"
-        cy.get('.woocommerce-message')
-          .should('be.visible')
-          .find('.button')
-          .click();
+        // ESPERA CRÍTICA: Aguarda a mensagem de sucesso aparecer antes de ir para o carrinho
+        // Aumentamos para 20s para garantir que o Jenkins processe a adição
+        cy.get('.woocommerce-message', { timeout: 20000 })
+            .should('be.visible')
+            .find('.button')
+            .click();
 
+        // Ir para o Checkout
         cy.get('.checkout-button').should('be.visible').click();
 
-        // No Checkout
-        // Marcar os termos e finalizar o pedido
+        // Finalização no Checkout
+        // O checkout do WooCommerce costuma ter muitos scripts, usamos force:true
         cy.get('#terms').click({ force: true });
         cy.get('#place_order').click({ force: true });
 
-        // Validação final com timeout estendido para o Jenkins
-        // O Jenkins pode demorar a processar o banco de dados do pedido
-        cy.get('.woocommerce-notice', { timeout: 20000 })
-          .should('be.visible')
-          .and('contain', 'recebido');
+        // Validação Final
+        cy.get('.woocommerce-notice', { timeout: 25000 })
+            .should('be.visible')
+            .and('contain', 'recebido');
     });
 });
