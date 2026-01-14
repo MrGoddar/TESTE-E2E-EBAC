a1f5207619a9065c8735a849ae526f2b3a79a3ca
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-14 16:22:37 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,5 @@
 Cypress.Commands.add('login', (usuario, senha) => {
-    cy.visit('minha-conta')
-    cy.get('#username').type(usuario, {force: true})
-    cy.get('#password').type(senha, {log: false, force: true})
-    cy.get('.woocommerce-form > .button').click({force: true})
+    cy.get('#username').type(usuario, { log: false });
+    cy.get('#password').type(senha, { log: false });
+    cy.get('.woocommerce-form-login__submit').click({ force: true });
 });
