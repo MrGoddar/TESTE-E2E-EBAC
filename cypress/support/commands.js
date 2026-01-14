caabca72bb009768ef737e7a4236d740bf187fa9
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-13 22:40:42 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,6 +3,6 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#password').type(senha, { log: false })
     cy.get('.woocommerce-form > .button').click()
 
-    // Isso garante que o Cypress espere o carregamento antes de prosseguir
+  
     cy.get('.page-title', { timeout: 15000 }).should('contain', 'Minha conta')
 });
