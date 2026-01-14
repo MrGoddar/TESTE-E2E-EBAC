25216b5b1df4836ee87b892293c1cdd4e19d38b6
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-14 15:44:08 -0300

    Update tentativa1.cy.js

--- cypress/e2e/tentativa1.cy.js
+++ cypress/e2e/tentativa1.cy.js
@@ -1,14 +1,1 @@
-/// <reference types="cypress" />
 
-describe('Funcionalidade: Adicionar múltiplos produtos', () => {
-    it('Deve adicionar produtos com login direto', () => {
-        cy.visit('minha-conta')
-        cy.login('matheuso54@hotmail.com', '@teste123')
-        cy.visit('produtos')
-        cy.get('.product-block').first().click()
-        cy.get('.button-variable-item-M').click()
-        cy.get('.button-variable-item-Blue').click()
-        cy.get('.single_add_to_cart_button').click()
-        cy.get('.woocommerce-message').should('contain', 'adicionado')
-    });
-});
