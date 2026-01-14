e27badd24b7c392a209cb8eeca2f51983fdbc6b4
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-13 22:54:18 -0300

    Update tentativa1.cy.js

--- cypress/e2e/tentativa1.cy.js
+++ cypress/e2e/tentativa1.cy.js
@@ -1,76 +1,14 @@
 /// <reference types="cypress" />
 
-describe('Funcionalidade: Adicionar múltiplos produtos ao carrinho', () => {
-
-
-    const produtosParaAdicionar = [
-        {
-            nome: 'Abominable Hoodie',
-            seletorProduto: '.post-2559',
-            seletorTamanho: '.button-variable-item-S',
-            seletorCor: '.button-variable-item-Red'
-        },
-        {
-            nome: 'Aero Daily Fitness Tee',
-            seletorProduto: '.post-3111',
-            seletorTamanho: '.button-variable-item-S',
-            seletorCor: '.button-variable-item-Brown'
-        },
-        {
-            nome: 'Arcadio Denim Overall', 
-            seletorProduto: '.post-3073',
-            seletorTamanho: '.button-variable-item-32', 
-            seletorCor: '.button-variable-item-Blue'
-        },
-        {
-            nome: 'Atomic Endurance Tee', 
-            seletorProduto: '.post-2622',
-            seletorTamanho: '.button-variable-item-S',
-            seletorCor: '.button-variable-item-Red'
-        }
-    ];
-    before(() => {
-        cy.session('carrinhoComQuatroProdutos', () => {
-           
-            cy.fixture('perfil').then(dados => {
-                cy.visit('/minha-conta'); 
-                cy.login(dados.usuario, dados.senha); 
-                cy.get('.woocommerce-MyAccount-content').should('contain', 'Olá, Aline');
-            });
-            produtosParaAdicionar.forEach(produto => {
-                cy.visit('/produtos/'); 
-                cy.get(produto.seletorProduto)
-                  .find('.product-image') 
-                  .click();
-
-                cy.get(produto.seletorTamanho).click();
-                cy.get(produto.seletorCor).click();
-                cy.get('.single_add_to_cart_button').click();
-             
-            });
-
-        }, {
-            validate: () => {
-                cy.visit('/carrinho/');
-                cy.get('.woocommerce-cart-form__contents .cart_item').should('have.length', produtosParaAdicionar.length);
-            }
-        });
+describe('Funcionalidade: Adicionar múltiplos produtos', () => {
+    it('Deve adicionar produtos com login direto', () => {
+        cy.visit('minha-conta')
+        cy.login('aluno_ebac@teste.com', 'teste@teste.com')
+        cy.visit('produtos')
+        cy.get('.product-block').first().click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.woocommerce-message').should('contain', 'adicionado')
     });
-
-    it('Deve adicionar todos os 4 produtos e verificar o carrinho', () => {
-        cy.visit('/carrinho/');
-        cy.get('.woocommerce-cart-form__contents .cart_item').should('have.length', produtosParaAdicionar.length);
-
-        produtosParaAdicionar.forEach((produto, index) => {
-            cy.get(`.cart_item:nth-child(${index + 1}) .product-name a`) 
-        });
-
-        cy.get('.checkout-button').should('be.visible');
-
-        cy.get('.checkout-button').click()
-        cy.get('#terms').click()
-        cy.get('#place_order').click()
-    });
-
-
 });
