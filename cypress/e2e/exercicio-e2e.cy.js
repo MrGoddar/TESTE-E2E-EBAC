ddadfd94e80de8ac40200bc1270bf89083770f73
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-14 17:18:03 -0300

    Update exercicio-e2e.cy.js

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -4,19 +4,28 @@ describe('Exercicio - Testes End-to-end', () => {
 
         // Seleção de produto estável
         cy.get('.product-block').first().click();
-        cy.get('.button-variable-item-S').click(); 
-        cy.get('.button-variable-item-Blue').click(); 
+        cy.get('.button-variable-item-S').click();
+        cy.get('.button-variable-item-Blue').click();
         cy.get('.single_add_to_cart_button').click();
 
-        // Em vez de classe CSS complexa, buscamos o texto do botão de ver carrinho
-        cy.contains('Ver carrinho').click(); 
-        cy.get('.checkout-button').click();
+        // Correção: Aguarda a mensagem de sucesso aparecer antes de buscar o botão
+        // Isso evita o erro de "content: Ver carrinho but never did"
+        cy.get('.woocommerce-message')
+          .should('be.visible')
+          .find('.button')
+          .click();
+
+        cy.get('.checkout-button').should('be.visible').click();
 
         // No Checkout
+        // Marcar os termos e finalizar o pedido
         cy.get('#terms').click({ force: true });
         cy.get('#place_order').click({ force: true });
 
-        // Validação com timeout estendido para o Jenkins
-        cy.get('.woocommerce-notice', { timeout: 20000 }).should('contain', 'recebido');
+        // Validação final com timeout estendido para o Jenkins
+        // O Jenkins pode demorar a processar o banco de dados do pedido
+        cy.get('.woocommerce-notice', { timeout: 20000 })
+          .should('be.visible')
+          .and('contain', 'recebido');
     });
 });
