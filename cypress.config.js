438e7b37520880890e846ec010afe371edc286fc
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-14 17:28:28 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,18 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
-    defaultCommandTimeout: 15000, // Aumentado para 15s para o Jenkins
-    pageLoadTimeout: 30000,
+    defaultCommandTimeout: 15000,
+    pageLoadTimeout: 60000,
+    video: false, // Desativar vídeo economiza CPU no Jenkins
+    screenshotOnRunFailure: true,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Isso ajuda a mitigar o erro de GPU que apareceu no seu log
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+          launchOptions.args.push('--disable-gpu');
+          return launchOptions;
+        }
+      });
     },
   },
 });
