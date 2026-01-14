8270e4b59db81e25fb075ae59eae94b7b0556f3b
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-14 16:59:06 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,7 @@
 Cypress.Commands.add('login', (usuario, senha) => {
-    // Garante que o campo de usuário esteja visível antes de digitar
     cy.get('#username', { timeout: 10000 }).should('be.visible').type(usuario);
     cy.get('#password').should('be.visible').type(senha);
     
-    // Tenta clicar no botão pelo nome da classe OU pelo atributo 'name'
-    cy.get('button[name="login"]').click({ force: true });
+    // Tenta pelo nome, se não achar, clica no botão que contém o texto "Login" ou "Acessar"
+    cy.get('button').contains(/Log in|Login|Acessar/i).click({ force: true });
 });
