4153e3cb3904b6ee234b4e8717939da107a94ac6
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-14 22:26:29 -0300

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,24 +1,14 @@
 describe('Funcionalidade Login', () => {
 
-    beforeEach(() => {
-        cy.visit('minha-conta/');
-    });
-
     it('Login com sucesso usando Comando customizado', () => {
-        // Substitua pelos seus dados válidos
-        cy.login('matheuso54@hotmail.com', '@teste123'); 
-        
-        // Verifica se o menu lateral de conta apareceu
-        cy.get('.woocommerce-MyAccount-navigation').should('be.visible');
-        
-        // Verifica se o link de Sair existe (indica que logou)
-        cy.contains('Sair').should('be.visible');
+        cy.login('matheuso54@hotmail.com', '@teste123')
+        cy.get('.page-title').should('contain', 'Minha conta')
     });
 
     it('Login usando fixture', () => {
         cy.fixture('perfil').then(dados => {
-            cy.login(dados.usuario, dados.senha);
-        });
-        cy.contains('Sair').should('be.visible');
+            cy.login(dados.usuario, dados.senha)
+            cy.get('.page-title').should('contain', 'Minha conta')
+        })
     });
 });
