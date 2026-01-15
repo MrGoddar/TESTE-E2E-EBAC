36fb155e1b8fcad5075b802e6c8ad704fc00df07
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-14 23:45:55 -0300

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,20 +1,14 @@
-// cypress/e2e/login.cy.js
-
 describe('Funcionalidade Login', () => {
 
     it('Login com sucesso usando Comando customizado', () => {
-        // Dados atualizados conforme sua imagem
         cy.login('matheuso54@hotmail.com', '@teste123')
-        
-        // Validação de sucesso
         cy.get('.page-title').should('contain', 'Minha conta')
-    })
+    });
 
     it('Login usando fixture', () => {
         cy.fixture('perfil').then(dados => {
-            // Aqui ele usará os dados que estiverem no seu arquivo perfil.json
             cy.login(dados.usuario, dados.senha)
             cy.get('.page-title').should('contain', 'Minha conta')
         })
-    })
-})
+    });
+});
