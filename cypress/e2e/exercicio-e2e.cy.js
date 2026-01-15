5abfe47acc5b2346dda9c06818bc80beb28c1531
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-15 00:08:44 -0300

    Update exercicio-e2e.cy.js

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -7,31 +7,31 @@ context('Exercicio - Testes End-to-end', () => {
     });
 
     it('Deve fazer um pedido de ponta a ponta', () => {
-        // Adicionando produtos usando o comando customizado
+        // 1. Adicionar produto
         cy.addProdutos('Abraxas Gym Pant', '32', 'Blue', 2)
         
-        // Espera a mensagem de sucesso aparecer antes de clicar no carrinho
-        cy.get('.woocommerce-message').should('contain', 'no seu carrinho')
+        // 2. Ir para o carrinho
         cy.get('.woocommerce-message > .button').click()
 
-        // Página de Carrinho -> Checkout
-        cy.get('.checkout-button').click()
+        // 3. Ir para Checkout (Garante que o botão está clicável)
+        cy.get('.checkout-button').should('be.visible').click()
 
-        // Preenchimento de Checkout com dados aleatórios
+        // 4. Preencher Checkout
         cy.get('#billing_first_name').type(faker.person.firstName())
         cy.get('#billing_last_name').type(faker.person.lastName())
         cy.get('#billing_address_1').type(faker.location.streetAddress())
         cy.get('#billing_city').type(faker.location.city())
         cy.get('#billing_postcode').type('01001-000')
-        cy.get('#billing_phone').type(faker.phone.number())
+        cy.get('#billing_phone').type(faker.phone.number('###########'))
         cy.get('#billing_email').type(faker.internet.email())
 
-        // Finalização
-        cy.get('#payment_method_cod').check()
-        cy.get('#terms').check()
-        cy.get('#place_order').click()
+        // 5. Finalizar (Uso do force:true para evitar bloqueios de UI)
+        cy.get('#payment_method_cod').check({force: true})
+        cy.get('#terms').check({force: true})
+        cy.get('#place_order').click({force: true})
 
-        // Validação final
-        cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+        // 6. Validação
+        cy.get('.woocommerce-notice', { timeout: 20000 })
+          .should('contain', 'Obrigado. Seu pedido foi recebido.')
     });
 });
