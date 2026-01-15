489014109d7a8989153773164ef3d97d60f0f9cc
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-15 00:09:03 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,15 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
-    defaultCommandTimeout: 15000, // Aumentado para lidar com a lentidão do site
+    video: false, // Desabilitar vídeo economiza memória no Jenkins
+    screenshotOnRunFailure: true,
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
     setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        if (browser.family === 'chromium' && browser.name !== 'electron') {
-          launchOptions.args.push('--disable-gpu');
-          launchOptions.args.push('--disable-dev-shm-usage');
-          return launchOptions;
-        }
-      });
+      // listeners
     },
   },
 });
