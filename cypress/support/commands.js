5b4bfc34a5c80edd2ff7df6c5ba7ed2f95433e83
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-15 00:08:13 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,18 +1,29 @@
 Cypress.Commands.add('login', (usuario, senha) => {
-    // Garante que está na página correta antes de tentar o login
-    cy.visit('minha-conta') 
+    cy.visit('minha-conta')
     
-    cy.get('#username').type(usuario, { log: false })
-    cy.get('#password').type(senha, { log: false })
+    // Espera explícita pelo campo para garantir que a página carregou
+    cy.get('#username', { timeout: 10000 })
+      .should('be.visible')
+      .type(usuario, { log: false })
+      
+    cy.get('#password')
+      .should('be.visible')
+      .type(senha, { log: false })
     
-    // Seletor ajustado para ser mais genérico caso o nome mude
-    cy.get('button[name="login"]').should('be.visible').click()
+    // Força o clique se houver algum overlay na frente (comum em CI)
+    cy.get('button[name="login"]').click({ force: true })
 })
 
 Cypress.Commands.add('addProdutos', (produto, tamanho, cor, quantidade) => {
     cy.get('.product-block').contains(produto).click()
-    cy.get('.variable-item-tuple-' + tamanho).click()
-    cy.get('.variable-item-tuple-' + cor).click()
+    
+    // Garante que as opções de variação estão visíveis
+    cy.get('.variable-item-tuple-' + tamanho).should('be.visible').click()
+    cy.get('.variable-item-tuple-' + cor).should('be.visible').click()
+    
     cy.get('.input-text').clear().type(quantidade)
     cy.get('.single_add_to_cart_button').click()
+    
+    // Validação de que o produto realmente entrou no carrinho antes de sair da página
+    cy.get('.woocommerce-message').should('contain', 'no seu carrinho')
 })
