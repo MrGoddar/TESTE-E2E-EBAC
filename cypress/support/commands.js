b0139bb17601a23d36ea2940d0552aa2ce3bd0bb
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-18 17:19:18 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,6 @@
 Cypress.Commands.add('login', (usuario, senha) => {
-    // Espera explícita pelo campo para garantir que a página carregou
-    cy.get('#username', { timeout: 15000 }).should('be.visible').type(usuario);
-    cy.get('#password', { timeout: 15000 }).should('be.visible').type(senha, { log: false });
-    
-    // Seleciona o botão de login e usa force: true para evitar bloqueios de UI
-    cy.get('.woocommerce-form-login__submit')
-      .should('be.visible')
-      .click({ force: true });
-});
+    cy.get('#username').type(usuario)
+    cy.get('#password').type(senha)
+    // Seletor alterado de .woocommerce-form-login__submit para input[name="login"]
+    cy.get('input[name="login"]').click()
+})
