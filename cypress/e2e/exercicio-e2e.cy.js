e42307d2142eb8b2505b000f1a103eec62c773d6
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-18 17:00:11 -0300

    Update exercicio-e2e.cy.js

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -8,38 +8,39 @@ context('Exercicio - Testes End-to-end', () => {
     });
 
     it('Deve fazer um pedido de ponta a ponta', () => {
-        // Seleciona o primeiro produto da lista
-        cy.get('.product-block').first().click()
+        // Seleciona o primeiro produto
+        cy.get('.product-block', { timeout: 10000 }).first().click()
         
-        // Seleciona atributos (Tamanho e Cor)
+        // Seleciona atributos
         cy.get('.variable-item').first().click()
         cy.get('.variable-item').last().click()
         
         cy.get('.input-text').clear().type('1')
         cy.get('.single_add_to_cart_button').click()
 
-        // CORREÇÃO: Clica no botão "Ver Carrinho" que aparece na mensagem de sucesso
+        // AJUSTE: Espera a mensagem de sucesso aparecer antes de buscar o botão
+        cy.get('.woocommerce-message', { timeout: 15000 }).should('be.visible')
         cy.get('.woocommerce-message > .button').click()
 
-        // Vai para o Checkout
-        cy.get('.checkout-button').click()
+        // Checkout
+        cy.get('.checkout-button').should('be.visible').click()
 
-        // Preenchimento dos dados (Faker)
+        // Dados de faturamento
         cy.get('#billing_first_name').type(faker.person.firstName())
         cy.get('#billing_last_name').type(faker.person.lastName())
         cy.get('#billing_address_1').type(faker.location.streetAddress())
         cy.get('#billing_city').type(faker.location.city())
         cy.get('#billing_postcode').type('01001-000')
-        cy.get('#billing_phone').type(faker.phone.number('11999999999'))
+        cy.get('#billing_phone').type('11999999999')
         cy.get('#billing_email').type(faker.internet.email())
 
-        // Finalização
+        // Finalização com force: true para garantir o clique no Jenkins
         cy.get('#payment_method_cod').check({force: true})
         cy.get('#terms').check({force: true})
         cy.get('#place_order').click({force: true})
 
-        // Validação final
-        cy.get('.woocommerce-notice', { timeout: 15000 })
+        // Validação
+        cy.get('.woocommerce-notice', { timeout: 20000 })
           .should('contain', 'Obrigado. Seu pedido foi recebido.')
     });
 });
