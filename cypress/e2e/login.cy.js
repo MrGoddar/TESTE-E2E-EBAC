a4d692bbd55373179adb4aca10d1a73f53b90f36
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-19 16:32:04 -0300

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,16 +5,6 @@ describe('Funcionalidade Login', () => {
 
     it('Login com sucesso usando Comando customizado', () => {
         cy.login('aluno_ebac@teste.com', 'teste@teste.com')
-        cy.get('.page-title').should('contain', 'Minha conta')
-    });
-
-    it('Login usando fixture', () => {
-        cy.fixture('perfil').then(dados => {
-            cy.get('#username').type(dados.usuario)
-            cy.get('#password').type(dados.senha)
-            // Seletor corrigido conforme a inspeção do elemento
-            cy.get('input[name="login"]').click()
-            cy.get('.page-title').should('contain', 'Minha conta')
-        })
+        cy.get('.page-title', { timeout: 10000 }).should('contain', 'Minha conta')
     });
 });
