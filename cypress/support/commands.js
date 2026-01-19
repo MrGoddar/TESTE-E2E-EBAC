6491a4a908d596f08bb87f0b5d191dd493f149d1
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-01-19 16:31:40 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
     cy.get('#password').type(senha)
-    // Seletor alterado de .woocommerce-form-login__submit para input[name="login"]
-    cy.get('input[name="login"]').click()
+    // Seletor corrigido com base na imagem do console
+    cy.get('input[name="login"]').click() 
 })
